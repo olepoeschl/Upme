5f03b633f4f797c7968687dd5026b597f6ad2790
olepoeschl <ole.poeschl@gmail.com>
2025-07-24 18:55:38 +0200

    red

--- src/test/java/com/olepoeschl/upme/WebResolverTest.java
+++ src/test/java/com/olepoeschl/upme/WebResolverTest.java
@@ -1,9 +1,12 @@
 package com.olepoeschl.upme;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebResolverTest {
 
+    @Test
     void testConstructorArgsWithGetters() {
         var resolver = new WebResolver("https://some.url.com/updates.json");
         assertEquals("https://some.url.com/updates.json", resolver.getUrl());
