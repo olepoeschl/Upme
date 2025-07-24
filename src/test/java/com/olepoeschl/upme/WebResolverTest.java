802a47a8076ddf21290c7dd0cf8af4ba69f424c2
olepoeschl <ole.poeschl@gmail.com>
2025-07-24 18:56:48 +0200

    red

--- src/test/java/com/olepoeschl/upme/WebResolverTest.java
+++ src/test/java/com/olepoeschl/upme/WebResolverTest.java
@@ -8,8 +8,13 @@ public class WebResolverTest {
 
     @Test
     void testConstructorArgsWithGetters() {
-        var resolver = new WebResolver("https://some.url.com/updates.json");
-        assertEquals("https://some.url.com/updates.json", resolver.getUrl());
+        var expectedUrl = "https://some.url.com/updates.json";
+        var resolver = new WebResolver(expectedUrl);
+        assertEquals(expectedUrl, resolver.getUrl());
+
+        expectedUrl = "https://another.url.com/updates.json";
+        resolver = new WebResolver(expectedUrl);
+        assertEquals(expectedUrl, resolver.getUrl());
     }
 
 }
