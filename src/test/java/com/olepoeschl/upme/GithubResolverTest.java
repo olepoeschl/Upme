6bdc5aba2ef7664a5b25992942143417f1ab061a
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2025-07-29 13:58:49 +0000

    red

--- src/test/java/com/olepoeschl/upme/GithubResolverTest.java
+++ src/test/java/com/olepoeschl/upme/GithubResolverTest.java
@@ -16,6 +16,8 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
