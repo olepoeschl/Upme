ca24929f30602cf52022b47b28ee42e29b09957b
olepoeschl <ole.poeschl@gmail.com>
2025-11-24 15:17:09 +0100

    added webdownloader test for no progress updates & made the test file much bigger for progress updates to be useful

--- src/test/java/com/olepoeschl/upme/WebDownloaderTest.java
+++ src/test/java/com/olepoeschl/upme/WebDownloaderTest.java
@@ -14,6 +14,7 @@ import java.nio.file.Paths;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDownloaderTest {
 
@@ -36,21 +37,61 @@ public class WebDownloaderTest {
     @Nested
     class DownloadUpdateTest {
         @Test
-        void testRegular() throws URISyntaxException, IOException {
+        void testWithProgressUpdates() throws URISyntaxException, IOException {
             byte[] expectedFileData = Files.readAllBytes(Paths.get(getClass().getResource("/WebDownloaderTest/regular.txt").toURI()));
 
             stubFor(get(urlEqualTo("/regular.txt"))
                 .willReturn(aResponse()
-                    .withBody(expectedFileData)));
+                    .withBody(expectedFileData)
+                    .withHeader("Content-Length", String.valueOf(expectedFileData.length))
+                    // the content-length header is crucial for progress updates, as otherwise the length is unknown,
+                    // hence progress cannot be calculated
+                ));
 
             Path downloadedFile = null;
             try (
                 WebDownloader downloader = new WebDownloader();
             ) {
-                downloadedFile = downloader.downloadUpdate(BASE_URL + "/regular.txt", progress -> {});
+                final float[] lastProgress = {0f};
+                downloadedFile = downloader.downloadUpdate(
+                    BASE_URL + "/regular.txt",
+                    progress -> lastProgress[0] = progress
+                );
                 byte[] actualFileData = Files.readAllBytes(downloadedFile);
 
                 assertArrayEquals(expectedFileData, actualFileData, "Actual file data does not match expected file data.");
+                assertEquals(1f, lastProgress[0], "Progress callback was not called with 100% progress.");
+
+            } finally {
+                if(downloadedFile != null)
+                    Files.deleteIfExists(downloadedFile);
+            }
+        }
+
+        @Test
+        void testShouldNotHaveProgressUpdates() throws URISyntaxException, IOException {
+            byte[] expectedFileData = Files.readAllBytes(Paths.get(getClass().getResource("/WebDownloaderTest/regular.txt").toURI()));
+
+            stubFor(get(urlEqualTo("/regular.txt"))
+                .willReturn(aResponse()
+                        .withBody(expectedFileData)
+                    // NO Content-Length header this time, so progress updates cannot be calculated
+                ));
+
+            Path downloadedFile = null;
+            try (
+                WebDownloader downloader = new WebDownloader();
+            ) {
+                final float[] lastProgress = {-1f};
+                downloadedFile = downloader.downloadUpdate(
+                    BASE_URL + "/regular.txt",
+                    progress -> lastProgress[0] = progress
+                );
+                byte[] actualFileData = Files.readAllBytes(downloadedFile);
+
+                assertArrayEquals(expectedFileData, actualFileData, "Actual file data does not match expected file data.");
+                assertEquals(-1f, lastProgress[0], "Progress callback should not have been called when content length is unknown.");
+
             } finally {
                 if(downloadedFile != null)
                     Files.deleteIfExists(downloadedFile);
